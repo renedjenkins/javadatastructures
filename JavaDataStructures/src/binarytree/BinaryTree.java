7427b3a8c879c1f247a7b6848be13f8dc753479c
renedjenkins <renedjenkins@gmail.com>
2018-07-22 19:52:32 -0500

    Testing commit from Linux box

--- JavaDataStructures/src/binarytree/BinaryTree.java
+++ JavaDataStructures/src/binarytree/BinaryTree.java
@@ -176,6 +176,7 @@ public class BinaryTree {
 		System.out.println("==================================================");
 		theTree.inOrderTraverseTree(theTree.root);
 		System.out.println("==================================================");
+		System.out.println("testing commit");
 	}
 
 }
